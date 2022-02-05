33ac605e858fd7c75c4c5726296b06aced77fb32
kt474 <kt474@cornell.edu>
2022-02-04 22:50:23 -0500

    add scramble select menu

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,7 +1,19 @@
 <template>
   <v-app-bar :value="hideNav" app color="primary" dark>
     <v-app-bar-nav-icon @click="openDrawer"></v-app-bar-nav-icon>
-    <v-toolbar-title class="text-h5 mr-3">Scramble: </v-toolbar-title>
+    <div class="d-flex align-center">
+      <v-toolbar-title class="text-h5 mr-3">Scramble: </v-toolbar-title>
+      <v-select
+        v-if="showSelect"
+        dense
+        height="20"
+        v-model="scrambleType"
+        :items="items"
+        @change="changeSelect"
+        class="mt-6 mr-4 scramble-select"
+        filled
+      ></v-select>
+    </div>
     <div class="d-flex justify-center align-center">
       <h5
         class="mr-5 text-h5"
@@ -18,6 +30,9 @@
 export default {
   name: "NavBar",
   data: () => ({
+    items: ["2x2", "3x3", "4x4"],
+    scrambleType: "3x3",
+    showSelect: true,
     drawer: true
   }),
   computed: {
@@ -38,6 +53,12 @@ export default {
     }
   },
   methods: {
+    changeSelect() {
+      this.showSelect = false;
+      this.$nextTick(() => {
+        this.showSelect = true;
+      });
+    },
     openDrawer() {
       this.drawer = !this.drawer;
       this.$emit("openDrawer", this.drawer);
@@ -46,4 +67,9 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+.scramble-select {
+  width: 6rem;
+  font-size: 1.25rem;
+}
+</style>
